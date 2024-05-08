d212d70c2b91ad8b755ec61303fce9ec15ed5555
Zkfi12 <168305566+Zkfi12@users.noreply.github.com>
2024-05-08 17:38:09 -0500

    Add files via upload

---
+++ 350-1_answers.py
@@ -0,0 +1,242 @@
+
+certs = {
+    "level1" : {
+        "True or False: From a security perspective, the best rooms are directly next to emergency exits.": "False",
+     
+        "From the following choices, select the factors you should consider to understand the threat in your environment.":"Choose all answers.",
+        
+        "IEDs may come in many forms and may be camouflaged to blend in to the surrounding environment. True or False?":"True.",
+     
+        "True or False: Surveillance can be performed through either stationary or mobile means.":"True",
+     
+        "True or False: Terrorists usually avoid tourist locations since they are not DOD-related.":"False",
+     
+        "True or False: Room invasions are a significant security issue for hotels located in CONUS.":"True",
+     
+        "Force Protection Condition DELTA means that your base is at which one of the following":"The most increased level of protection",
+    
+        "True or False: In the event of a skyjacking, you should immediately attempt to subdue the skyjackers.": "False.",
+     
+        "Persons who have been given access to an installation can be counted on to be of no threat. True or False?": "False",
+     
+        "Select the factors that will help you avoid becoming the victim of a terrorist attack.": "All (Predictability, Location, Opportunity and Association)",
+     
+        "True or False: Security is a team effort.": "True.",
+     
+        "True or False: The initial moments of a hostage taking incident can be extremely dangerous.": "True.",
+         
+        "Knowing indicators of an unstable person can allow you to identify a potential insider threat before an incident.": "True.",
+     
+        "True or False: Active resistance should be the immediate response to an active shooter incident.": "False.",
+     
+        "Which one of the following is NOT an early indicator of a potential insider threat?": "A reasonable disagreement with a US Government Policy.",
+     
+        "Electronic audio and video devices are never used by terrorists for surveillance purposes. True or False?": "False.",
+     
+        "Which of the following is not a useful vehicle feature from a security perspective?": "Air bags.",
+     
+        "True or False: In an active shooter incident involving firearms you should immediately lie on the ground.": "False.",
+     
+        "True or False: When possible, it is best to always travel with a cell phone.": "True.",
+     
+        "True or False: State Department Travel Warnings should be consulted prior to taking trips across the US-Mexican border.": "True.",
+     
+        "True or False: Everyone on an installation has shared responsibility for security.": "True.",
+     
+        "Alerts from the National Terrorism Advisory System apply only to the United States and its possessions.": "True.",
+     
+        "True or False: The ticketing area is more secure than the area beyond the security check point.": "False.",
+     
+        "Home security can be improved with self-help measures like changing locks, securing windows, and improving outdoor lighting. True or False?": "True",
+     
+        "If you identify a possible surveillance attempt you should try to handle the situation yourself.": "False.",
+    
+        "True or False: Internet acquaintances can pose a security threat and should be carefully monitored.": "True.",
+     
+        "Early symptoms of a biological attack may appear the same as common illnesses. True or False?": "True.",
+     
+        "True or False: Reasons for acquiring hostages include publicity, use as a bargaining chip while executing other crimes, the forcing of political concessions, and ransom.": "True.",
+     
+        "Which of the following have NOT been targeted or plotted against by terrorists or violent individuals? (Antiterrorism Scenario Training, Page 1)": "none of these answers.",
+     
+        "Which one of these is a possible indicator of a suspicious letter or package? (Antiterrorism Scenario Training, Page 4)": "Misspellings of common words",
+     
+        "Which of the following is NOT a recommended response to an active shooter incident? (Antiterrorism Scenario Training, Pages 3 and 4": "Provide instructions to arriving emergency response personnel",
+     
+        "Keeping a well-maintained vehicle is considered a ""best practice"" from both a security and safety perspective. True or False? (Antiterrorism Scenario Training, Page 2)": "True.",
+     
+        "Select all factors that are ways in which you might become the victim of a terrorist attack. (Introduction to Antiterrorism, Page 4)": "All",
+     
+        "What should you NOT do during a hostage rescue attempt? (Antiterrorism Scenario Training, Page 4)": "Try to assist hostage rescue team",
+     
+        "From an antiterrorism perspective, espionage and security negligence are considered insider threats. (Antiterrorism Scenario Training, Page 2)": "True.",
+     
+        "Which one of these does NOT pose a risk to security at a government facility? (Antiterrorism Scenario Training, Page 2)": "A person expressing boredom with the US mission",
+     
+        "Which of the following is NOT an Antiterrorism Level I theme? (Antiterrorism Scenario Training, Page 2)": "Counter-surveillance",       
+    },
+    
+    "cyber_awareness": {
+        "What conditions are necessary to be granted access to Sensitive Compartmented Information (SCI)?": "Top Secret clearance and indoctrination into the SCI program",
+        
+        "Which of the following is permitted when using an unclassified laptop within a collateral classified space?": "A Government-issued wired headset with microphone",
+        
+        "Which of the following is an authoritative source for derivative classification?": "Security Classification Guide",
+        
+        "Carl receives an e-mail about a potential health risk caused by a common ingredient in processed food. Which of the following actions should Carl NOT take with the e-mail?": "Forward it",
+        
+        "How can an adversary use information available in public records to target you?": "Combine it with information from other data sources to learn how best to bait you with a scam",
+        
+        "Which of the following is an appropriate use of government e-mail?": "Using a digital signature when sending attachments",
+        
+        "Which of the following is NOT a best practice for protecting data on a mobile device?": "Disable automatic screen locking after a period of inactivity",
+        
+        "Annabeth becomes aware that a conversation with a co-worker that involved Sensitive Compartmented Information (SCI) may have been overheard by someone who does not have the required clearance. What action should Annabeth take?": "Contact her security POC to report the incident.",
+        
+        "On your home computer, how can you best establish passwords when creating separate user accounts?": "Have each user create their own, strong password",
+        
+        "Which of the following is an allowed use of government furnished equipment (GFE)?": "Checking personal e-mail if your organization allows it",
+        
+        "How can you prevent viruses and malicious code?": "Scan all external files before uploading to your computer",
+        
+        "Which best describes an insider threat? Someone who uses __________ access, ___________, to harm national security through unauthorized disclosure, data modification, espionage, terrorism, or kinetic actions.": "authorized, wittingly or unwittingly",
+        
+        "Which of the following is an example of behavior that you should report?": "Taking sensitive information home for telework without authorization",
+        
+        "Which of the following is true of telework?": "You must have permission from your organization to telework.",
+        
+        "After a classified document is leaked online, it makes national headlines. Which of the following statements is true of the leaked information that is now accessible by the public?": "You should still treat it as classified even though it has been compromised.",
+        
+        "How should government owned removable media be stored?": "In a GSA-approved container according to the appropriate security classification",
+        
+        "When linked to a specific individual, which of the following is NOT an example of Personally Identifiable Information (PII)?": "Automobile make and model",
+        
+        "What does the Common Access Card (CAC) contain?": "Certificates for identification, encryption, and digital signature",
+        
+        "Sylvia commutes to work via public transportation. She often uses the time to get a head start on work by making phone calls or responding to e-mails on her government approved mobile device. Does this pose a security concern?": "Yes. Eavesdroppers may be listening to Sylvia's phone calls, and shoulder surfers may be looking at her screen. Sylvia should be aware of these risks.",
+        
+        "Beth taps her phone at a payment terminal to pay for a purchase. Does this pose a security risk?": "Yes, there is a risk that the signal could be intercepted and altered.",
+        
+        "Which of the following is NOT an appropriate use of your Common Access Card (CAC)?": "Using it as photo identification with a commercial entity",
+        
+        "When is the safest time to post on social media about your vacation plans?": "After the trip",
+        
+        "You receive a text message from a package shipper notifying you that your package delivery is delayed due to needing updated delivery instructions from you. It provides a shortened link for you to provide the needed information. You are not expecting a package. What is the best course of action?": "Delete the message",
+        
+        "Which of the following is NOT a best practice for protecting your home wireless network for telework?": "Use your router's pre-set Service Set Identifier (SSID) and password",
+        
+        "Which of the following is a best practice for using government e-mail?": "Do not send mass e-mails",
+        
+        "Your meeting notes are unclassified. This means that your notes": "Do not have the potential to damage national security.",
+        
+        "What type of information does this personnel roster represent": "Controlled unclassified information (cui)",
+        
+        "When e-mailing this personnel roster, which of the following should you do?": "All 3- encrypt, digitally, use",
+        
+        "Select an action to take in response to compromised sensitive compartmented information (sci)": "Call your security point of contact (poc)",
+        
+        "Select a clue/ laptop- waterjug- printer": "aptop-yes/ waterjug-no/ printer- no",
+        
+        "which of these individuals demonstrated behavior that could lead to the compromise of SCI": "Col. Cockatiel",
+        
+        "which of the following poses a physical security risk.": "All 3 choices",
+        
+        "is this an appropriate use of government-furnished equipment (gfe) sending gov email selling cookies": "no/ all 3 you choices",
+        
+        "Select the individual who securely authenticates their identity": "Alex. she is her",
+        
+        "How can malicious code spread": "all except virus scans",
+        
+        "How can you prevent the download of malicious code": "Scan external files before uploading toy your device Research apps and their vulnerabilities before downloading",
+        
+        "which of the following may indicate a malicious code attack": "a new app suddenly appears appears on the device, the device slows down, a new tap appears in the web browser",
+        
+        
+        
+        
+        
+    },
+        
+    "infosec": {
+        "The Tier 3 Investigation is designated for the following positions:": "Non-critical sensitive.",
+        
+        "Which of the following methods may be used to transmit Top Secret material?": "- Appropriately cleared courier, - Protected facsimile, message or voice",
+        
+        "Which of the following is responsible for the review of written materials for public release?": "Defense Office of Republication and Security Review",
+        
+        "Good Operations Security (OPSEC) practices DO NOT include:": "Discussing sensitive information carefully in public",
+        
+        "The Physical Security Program is designed to:": "Protect against espionage, sabotage, damage, and theft",
+        
+        "All of the following are examples of Adverse Information that must be reported EXCEPT:": "Traffic violations with a fine under $300",
+        
+        "You must obtain a defensive foreign travel security briefing prior to travel or at least once a year from whom?": "Security Office",
+        
+        "What form is used to record the opening and closing of the security container?": "SF702, Security Container Check Sheet",
+        
+        "In addition to foreign travel requirements, those with SCI access must:": "- Complete a foreign travel questionnaire prior to proceeding on travel, - Complete a secondary questionnaire upon return.",
+        
+    }    
+}
+
+
+def main_menu():
+    while True:
+        
+         print("Welcome to the 350-1 helper.\n")
+         print("1. Level 1 Anti-Terrorism")
+         print("2. Cyber Awareness")
+         print("3. InfoSec")
+         user_choice = input("Please enter the number corresponding to the test you would like the answers to:")
+         if user_choice == "1":
+             anti_terrorism()
+             break
+         elif user_choice == "2":
+             cyber_awareness()
+             break
+         elif user_choice == "3":
+             infosec()
+             break
+         else:
+             print("Please enter a valid choice.")
+        
+
+def anti_terrorism():
+    while True:
+         print("LEVEL 1 ANTI-TERRORISM\n")
+         user_input = input("Please copy and paste the question here, then press enter to recieve the answer: ")
+         if user_input in certs["level1"]:
+             print("Answer: ", certs["level1"][user_input])
+             
+         else:
+             print("Question not found, try another question.")
+             
+def cyber_awareness():
+    while True:
+        print("\n")
+        print("CYBER AWARENESS\n")
+        print("DISCLAIMER!! YOU MUST COPY AND PASTE THE QUESTION.\n")
+        user_input = input("Please copy and paste the question here, then press enter to recieve the answer: ")
+        if user_input in certs["cyber_awareness"]:
+            print("Answer: ", certs["cyber_awareness"][user_input])
+            
+        else:
+            print("Question not found, try another question.")
+            
+def infosec():
+    while True:
+        print("INFOSEC\n")
+        print("DISCLAIMER!! YOU MUST COPY AND PASTE THE QUESTION.\n")
+        user_input = input("Please copy and paste the question here, then press enter to recieve the answer: ")
+        if user_input in certs["infosec"]:
+            print("Answer: ", certs["infosec"][user_input])
+            
+        else:
+            print("Question not found, try another question.")
+             
+        
+    
+
+
+
+main_menu()
